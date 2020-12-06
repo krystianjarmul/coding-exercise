7e0f4f77c64a2f9bfc84c1cbe24a833dac121992
krystianjarmul <krystianjarmul.96@gmail.com>
2020-12-07 00:26:13 +0100

    Add material attribute to Bathtub class

--- bathtub.py
+++ bathtub.py
@@ -4,9 +4,10 @@ import typing as tp
 class Bathtub:
     """Class representing a bathtub object"""
 
-    def __init__(self, colour: str, brand: str) -> None:
+    def __init__(self, colour: str, brand: str, material: str) -> None:
         self.colour = colour
         self.brand = brand
+        self.material = material
 
     @property
     def colour(self) -> str:
@@ -27,3 +28,13 @@ class Bathtub:
         if not isinstance(value, str):
             raise ValueError
         self._brand = value
+
+    @property
+    def material(self) -> str:
+        return self._material
+
+    @material.setter
+    def material(self, value: tp.Any) -> None:
+        if not isinstance(value, str):
+            raise ValueError
+        self._material = value
