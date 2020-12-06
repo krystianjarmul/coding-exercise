6e9418a2c851bea50a4eb45dc108db8d31e6236c
krystianjarmul <krystianjarmul.96@gmail.com>
2020-12-07 00:09:08 +0100

    Add test for correct value of brand attribute

--- test_bathtub.py
+++ test_bathtub.py
@@ -7,13 +7,20 @@ class BathtubTests(unittest.TestCase):
     """Tests for a bathtub object"""
 
     def setUp(self):
-        self.attrs = {'colour': 'white'}
+        self.attrs = {'colour': 'white',
+                      'brand': 'Test'}
 
     def test_colour(self):
         """Test that raise ValueError if colour of bathtub is not a string"""
         self.attrs['colour'] = 123
         with self.assertRaises(ValueError):
-            bathtub = Bathtub(**self.attrs)
+            Bathtub(**self.attrs)
+
+    def test_brand(self):
+        """Test that raise ValueError if brand of bathtub is not a string"""
+        self.attrs['brand'] = []
+        with self.assertRaises(ValueError):
+            Bathtub(**self.attrs)
 
 
 if __name__ == '__main__':
