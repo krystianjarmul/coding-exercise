5605bf03e6dee9f54ca85f359a52e22b7d5187a2
krystianjarmul <krystianjarmul.96@gmail.com>
2020-12-07 00:41:05 +0100

    Add weight attribute to Bathtub class

--- bathtub.py
+++ bathtub.py
@@ -4,10 +4,12 @@ import typing as tp
 class Bathtub:
     """Class representing a bathtub object"""
 
-    def __init__(self, colour: str, brand: str, material: str) -> None:
+    def __init__(self, colour: str, brand: str, material: str,
+                 weight: int) -> None:
         self.colour = colour
         self.brand = brand
         self.material = material
+        self.weight = weight
 
     @property
     def colour(self) -> str:
@@ -38,3 +40,13 @@ class Bathtub:
         if not isinstance(value, str):
             raise ValueError
         self._material = value
+
+    @property
+    def weight(self) -> int:
+        return self._weight
+
+    @weight.setter
+    def weight(self, value: tp.Any) -> None:
+        if not isinstance(value, int):
+            raise ValueError
+        self._weight = value
