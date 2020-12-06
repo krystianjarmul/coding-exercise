d664577d592b133abfc23ac1fc29aba87ebd4943
krystianjarmul <krystianjarmul.96@gmail.com>
2020-12-06 17:37:30 +0100

    Add test for correct value of colour attribute

---
+++ test_bathtub.py
@@ -0,0 +1,18 @@
+import unittest
+
+
+class BathtubTests(unittest.TestCase):
+    """Tests for a bathtub object"""
+
+    def setUp(self):
+        self.attrs = {'colour': 'white'}
+
+    def test_colour(self):
+        """Test that raise ValueError if colour of bathtub is not a string"""
+        self.attrs['colour'] = 123
+        with self.assertRaises(ValueError):
+            bathtub = Bathtub(**self.attrs)
+
+
+if __name__ == '__main__':
+    unittest.main()
