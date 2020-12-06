b9a662c84ba865733d93043dabef9d6655228e65
krystianjarmul <krystianjarmul.96@gmail.com>
2020-12-07 00:16:21 +0100

    Add brand attribute to Bathtub class

--- bathtub.py
+++ bathtub.py
@@ -1,15 +1,29 @@
+import typing as tp
+
+
 class Bathtub:
     """Class representing a bathtub object"""
 
-    def __init__(self, colour: str) -> None:
+    def __init__(self, colour: str, brand: str) -> None:
         self.colour = colour
+        self.brand = brand
 
     @property
-    def colour(self):
+    def colour(self) -> str:
         return self._colour
 
     @colour.setter
-    def colour(self, value):
+    def colour(self, value: tp.Any) -> None:
         if not isinstance(value, str):
             raise ValueError
         self._colour = value
+
+    @property
+    def brand(self) -> str:
+        return self._brand
+
+    @brand.setter
+    def brand(self, value: tp.Any) -> None:
+        if not isinstance(value, str):
+            raise ValueError
+        self._brand = value
